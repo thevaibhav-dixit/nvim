c1211535522cdcd85bca565ceba6d31184301ee7
Vaibhav <thevaibhavdixit@gmail.com>
2023-12-19 09:14:02 +0530

    feat: <CR> to accept suggestions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -22,3 +22,10 @@ require('rust-tools').setup({
         },
     },
 })
+
+local cmp = require('cmp')
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		['<CR>'] = cmp.mapping.confirm({ select = true }), 
+	}),
+})
