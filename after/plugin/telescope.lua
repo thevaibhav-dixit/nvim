54db221503fc2789bf9913ff6ea3840a66d85540
Vaibhav <thevaibhavdixit@gmail.com>
2024-01-20 12:45:40 +0530

    chore: add layout strategy for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,25 @@ local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 
+require('telescope').setup {
+  defaults = {
+    previewer = true,
+		layout_strategy = 'bottom_pane',
+    layout_config = {
+      width = 0.99,
+      height = 0.6,
+      prompt_position = "bottom",
+      horizontal = {
+        preview_width = 0.5,
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+    },
+  },
+}
+
 local function open_in_right_vertical_split(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   actions.close(prompt_bufnr)
